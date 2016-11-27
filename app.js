1787b96d90e092f0d84f2948669b83df8e5d2ee2
destino92 <destinodello@gmail.com>
2016-11-28 02:38:49 +0300

    chore: app.js skeleton initializing a Vue.js instance

---
+++ app.js
@@ -0,0 +1,16 @@
+new Vue({
+  // Target div with id of 'events'
+  el: '#events',
+
+  // container for values or collections that hold data
+  // for the application.
+  data: {},
+
+  // Anything within the ready function will run when
+  // the app loads.
+  ready: function() {},
+
+  // Methods we want to use in our application are
+  // registered here
+  methods: {}
+});
